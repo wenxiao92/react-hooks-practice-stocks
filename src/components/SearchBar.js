f7a4d1758c0c638a8de78528f964a5cf926ee2a2
Wen Xiao <wennstyle@gmail.com>
2022-02-17 23:27:01 -0500

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,17 @@
 import React from "react";
 
-function SearchBar() {
+function SearchBar({sort, onChangeSort, filterBy, onChangeFilter}) {
+
+  function handleSortChange(event){
+    event.preventDefault()
+    onChangeSort(event.target.value)
+  }
+
+  function handleFilterChange(event){
+    event.preventDefault()
+    onChangeFilter(event.target.value)
+  }
+
   return (
     <div>
       <strong>Sort by:</strong>
@@ -9,8 +20,8 @@ function SearchBar() {
           type="radio"
           value="Alphabetically"
           name="sort"
-          checked={null}
-          onChange={null}
+          checked={sort === "Alphabetically"}
+          onChange={handleSortChange}
         />
         Alphabetically
       </label>
@@ -19,15 +30,15 @@ function SearchBar() {
           type="radio"
           value="Price"
           name="sort"
-          checked={null}
-          onChange={null}
+          checked={sort === "Price"}
+          onChange={handleSortChange}
         />
         Price
       </label>
       <br />
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={handleFilterChange} value={filterBy}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,13 +1,23 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({stocks, onAddToPortfolio}) {
+
+  const displayStocks = stocks.map((eachStock) => (
+    <Stock
+    key={eachStock.id}
+    stockList={eachStock}
+    onStockClick={onAddToPortfolio}
+    />
+  ))
+
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {displayStocks}
     </div>
   );
 }
 
 export default StockContainer;
+
